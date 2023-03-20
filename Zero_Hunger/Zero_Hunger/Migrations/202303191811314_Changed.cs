06316d830af1e67b80542a3721fefb7b7bca35b4
Rubaiyat Rahman <94232498+Rubaiyat90@users.noreply.github.com>
2023-03-20 20:24:22 +0600

    V1

---
+++ Zero_Hunger/Zero_Hunger/Migrations/202303191811314_Changed.cs
@@ -0,0 +1,20 @@
+﻿namespace Zero_Hunger.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Changed : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.DistributionDetails", "ResId", c => c.String(nullable: false));
+            DropColumn("dbo.DistributionDetails", "ResTurantName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.DistributionDetails", "ResTurantName", c => c.String(nullable: false));
+            DropColumn("dbo.DistributionDetails", "ResId");
+        }
+    }
+}
